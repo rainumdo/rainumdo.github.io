b04d1b14caa02be867941898b7546c09daba0ffe
rainumdo <rainumdo@qq.com>
2024-01-24 10:43:41 +0800

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240123.233208';
+const cacheName = 'chirpy-20240124.104323';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
